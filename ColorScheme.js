1e8e91cad9803b31e9d38c73b999d9f1ad7cfd4f
Roman Riesen <rriesen@mcscorp.ch>
2017-04-27 13:13:40 +0200

    canges done.

--- ColorScheme.js
+++ ColorScheme.js
@@ -33,8 +33,8 @@ function DarkColorScheme(){
     this.neutralColor      =    'rgba(80, 80, 80, 1)'
     this.backgroundImage   =    ""
     this.backgroundColor   =    'rgba(0,0,0,0.0)'
-    this.fieldColor        =    'rgba(0, 0, 0, 0.53)'
-    this.fieldBorderColor  =    'rgb(255,0,0)'
+    this.fieldColor        =    'rgba(60, 60, 60, 0.53)'
+    this.fieldBorderColor  =    'rgb(22,22,22)'
     this.crownColor        =    'rgb(255, 0, 0)'
 }   
 
