f4286fc451bb5420fd347c6b91c9a54d1975947b
Roman Riesen <rriesen@mcscorp.ch>
2017-05-09 16:03:07 +0200

    Some more gui adjustments

--- ColorScheme.js
+++ ColorScheme.js
@@ -1,5 +1,5 @@
 //colourscheme
-function BWScheme(){ 
+function BWScheme(){
     this.player1color1     =    'rgba(0, 0, 0, 1)'
     this.player1color2     =    'rgba(255, 255, 255, 1)'
     this.player2color1     =    'rgba(255, 255, 255, 1)'
@@ -10,9 +10,9 @@ function BWScheme(){
     this.fieldColor        =    'rgba(60, 60, 60,0.5)'
     this.fieldBorderColor  =    'rgb(20,20,20)'
     this.crownColor        =    'rgb(255,0,0)'
-}   
+}
 
-function DefaultScheme(){   
+function DefaultScheme(){
     this.player1color1     =    'rgb(240, 130, 0)'
     this.player1color2     =    'rgb(255, 240, 70)'
     this.player2color1     =    'rgb(50, 103, 222)'
@@ -23,22 +23,22 @@ function DefaultScheme(){
     this.fieldColor        =    'rgba(60, 60, 60,0.5)'
     this.fieldBorderColor  =    'rgb(20,20,20)'
     this.crownColor        =    'rgb(255,0,0)'
-}   
+}
 
-function DarkColorScheme(){ 
+function DarkColorScheme(){
     this.player1color2     =    'rgb(0, 250, 0)'
-    this.player1color1     =    'rgb(0,0,0)'
+    this.player1color1     =    'rgb(40,40,40)'
     this.player2color2     =    'rgb(50, 200, 255)'
-    this.player2color1     =    'rgb(0,0,0)'
-    this.neutralColor      =    'rgba(30, 30, 30, 0.8)'
+    this.player2color1     =    'rgb(40,40,40)'
+    this.neutralColor      =    'rgb(40,40,40)'//'rgba(30, 30, 30, 0.8)'
     this.backgroundImage   =    ""
-    this.backgroundColor   =    'rgba(0,0,0,0.0)'
-    this.fieldColor        =    'rgba(60, 60, 60, 0.53)'
+    this.backgroundColor   =    'rgb(40,40,40)'
+    this.fieldColor        =    'rgba(80, 80, 80, 0.53)'
     this.fieldBorderColor  =    'rgba(0,0,0,0)'
     this.crownColor        =    'rgb(255, 0, 90)'
-}   
+}
 
-function TronColorScheme(){ 
+function TronColorScheme(){
     this.player1color2     =    'rgb(255, 191, 0)'
     this.player1color1     =    'rgb(0,0,0)'
     this.player2color2     =    'rgb(50, 200, 255)'
@@ -49,9 +49,9 @@ function TronColorScheme(){
     this.fieldColor        =    'black'
     this.fieldBorderColor  =    'rgba(200, 20,20, 0.4)'
     this.crownColor        =    'rgb(255, 0, 0)'
-}   
+}
 
-function NeonColorScheme(){ 
+function NeonColorScheme(){
     this.player1color2     =    'rgb(0, 191, 0)'
     this.player1color1     =    'rgb(191,0,100)'
     this.player2color2     =    'rgb(191,0,100)'
@@ -62,7 +62,7 @@ function NeonColorScheme(){
     this.fieldColor        =    'rgba(255,255,255,0.3)'
     this.fieldBorderColor  =    'rgba(100,100,155,0.4)'//'rgb(20, 120, 150)'
     this.crownColor        =    'rgba(0,100,191,1)'//'rgb(20, 120, 150)'
-}   
+}
 
 
 
@@ -76,14 +76,3 @@ colorSchemeDict = {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
