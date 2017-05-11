8c82357903d26af993312cb444a207c7015d563e
RomanRiesen <roman.r97@gmx.ch>
2017-05-11 21:48:55 +0200

    This commit message is the default used by my script.

--- commit.sh
+++ commit.sh
@@ -1,7 +1,6 @@
 # This script git-commits all files and uploads them to the dev branch, whilst also minifying them and uploading them to the master branch.
 git checkout dev
 git commit -a -m "This commit message is the default used by my script."
-git push
 git checkout master
 git commit -a -m "This commit message is the default used by my script."
 git merge dev
